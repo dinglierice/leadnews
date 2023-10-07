0de4523f3e30432c5abd4c87298854683a3f5821
dinglierice <dinglie1997@163.com>
2023-10-07 22:27:02 +0800

    测试

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,9 @@
+package com.heima.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.common.constants.ArticleConstants;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface ApArticleContentMapper extends BaseMapper<ArticleConstants> {
+}
