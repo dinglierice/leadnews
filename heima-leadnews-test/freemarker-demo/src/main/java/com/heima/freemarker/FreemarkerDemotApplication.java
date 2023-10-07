d442b053967b9ac7e6acd337e8088269fe01d9b9
dinglierice <dinglie1997@163.com>
2023-10-07 19:44:44 +0800

    before minio

---
+++ heima-leadnews-test/freemarker-demo/src/main/java/com/heima/freemarker/FreemarkerDemotApplication.java
@@ -0,0 +1,16 @@
+package com.heima.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description：TODO
+ * @author：dinglie
+ * @date：2023/10/7 16:38
+ */
+@SpringBootApplication
+public class FreemarkerDemotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerDemotApplication.class, args);
+    }
+}
