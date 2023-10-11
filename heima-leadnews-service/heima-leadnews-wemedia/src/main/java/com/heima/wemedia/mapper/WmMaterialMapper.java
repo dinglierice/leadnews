2811eaab4ccb9f4b3736a61dae4b4732c66ec05d
dinglierice <dinglie1997@163.com>
2023-10-12 00:09:36 +0800

    查询

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/mapper/WmMaterialMapper.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/mapper/WmMaterialMapper.java
@@ -2,6 +2,8 @@ package com.heima.wemedia.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.heima.model.wemedia.pojos.WmMaterial;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface WmMaterialMapper extends BaseMapper<WmMaterial> {
 }
